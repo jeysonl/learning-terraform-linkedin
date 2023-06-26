a66a9eb618ae09938863e30a5d29cfdaf6821a8c
jeysonl <45678116+jeysonl@users.noreply.github.com>
2023-06-26 17:35:55 -0500

    autoscaling

--- main.tf
+++ main.tf
@@ -29,18 +29,28 @@ module "vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
+name = "blog"
+min_size = 1
+max_size = 2
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+vpc_zone_identifier = module.vpc.public_subnets
+target_group_arns = module.blog_alb.target_group_arns
+security_groups = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "HelloWorld"
-  }
+image_id           = data.aws_ami.app_ami.id
+instance_type = var.instance_type
+
+
 }
 
-module "alb" {
+
+
+
+
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
