13e0cfccd17e5d660fd9e191ad6b978091cbd580
jeysonl <45678116+jeysonl@users.noreply.github.com>
2023-06-26 17:27:41 -0500

    final_

--- main.tf
+++ main.tf
@@ -48,8 +48,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.dev.vpc_id
-  subnets            = module.dev.public_subnets
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
   access_logs = {
